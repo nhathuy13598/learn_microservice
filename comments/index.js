d63b3e2e224e78b8296ae42800117199cac597e6
nhathuy <nhathuy13598@gmail.com>
2023-06-11 17:03:14 +0700

    Init

---
+++ comments/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+
+app.post("/posts/:id/comments", async (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const {content} = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({id: commentId, content});
+    commentsByPostId[req.params.id] = comments;
+
+    await axios.post('http://localhost:4005/events', {
+        type: "CommentCreated",
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    }).catch((err) => console.log(err.message));
+    res.status(201).send(comments);
+});
+
+app.get("/posts/:id/comments", (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/events", (req, res) => {
+    console.log("Recieve event", req.body.type);
+    res.send({});
+});
+
+app.listen(4001, () => {
+    console.log("Listening on 4001");
+});
