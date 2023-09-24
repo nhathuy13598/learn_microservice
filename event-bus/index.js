253ba68232f5209f8cfbd9668dace99d1b9c8419
nhathuy <nhathuy13598@gmail.com>
2023-09-24 16:38:52 +0700

    Set up k8s

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,12 +9,13 @@ const events = [];
 
 app.post('/events', (req, res) => {
     const event = req.body;
+    console.log("Recieve event", event.type);
     events.push(event);
 
-    axios.post('http://localhost:4000/events', event).catch((err) => console.log(err.message));
-    axios.post('http://localhost:4001/events', event).catch((err) => console.log(err.message));
-    axios.post('http://localhost:4002/events', event).catch((err) => console.log(err.message));
-    axios.post('http://localhost:4003/events', event).catch((err) => console.log(err.message));
+    axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => console.log(err.message));
+    axios.post('http://comments-srv:4001/events', event).catch((err) => console.log(err.message));
+    axios.post('http://query-srv:4002/events', event).catch((err) => console.log(err.message));
+    axios.post('http://moderation-srv:4003/events', event).catch((err) => console.log(err.message));
     res.send({status: 'OK'});
 });
 
